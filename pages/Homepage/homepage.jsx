cfe12c0ffeb3e412cb7e3e012c5d8b233d64df7a
Hashicy <harshita.joshi.1505@gmail.com>
2025-05-04 09:05:20 +0530

    commiting again

---
+++ pages/Homepage/homepage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from '../common/header'
+
+function Homepage() {
+  return (
+   <Header/>
+  )
+}
+
+export default Homepage
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Poppins } from "next/font/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,6 +20,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+
+      <head>
+        <link href="https://fonts.googleapis.com/css2?family=Meddon&display=swap" rel="stylesheet" />
+      </head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >

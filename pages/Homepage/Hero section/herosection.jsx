991ac1b16fa484bfb96f422fdd6a42c29f1044bd
Hashicy <harshita.joshi.1505@gmail.com>
2025-05-06 10:05:05 +0530

    commitement is the key

--- pages/Homepage/Hero section/herosection.jsx
+++ pages/Homepage/Hero section/herosection.jsx
@@ -7,12 +7,8 @@ export default function Hero() {
   <p>Seamless and Transparent Solutions for Every Indian</p>
   <p className="description">EZY Tax is your next-generation platform designed to simplify tax filing, maximize your savings, and ensure complete peace of mind.</p>
   <div className="hero-buttons">
-    <button className="primary-btn">Get Started</button>
+  <button className="primary-btn">Get Started</button>
   <a href="#Services"><button className="secondary-btn">Learn More</button></a>
-
-  </div>
-  <div className="hero-image">
-   
   </div>
 </div>
   );

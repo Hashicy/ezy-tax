d075fa79407e976bfbc26ceac1b64c6555c2a539
Hashicy <harshita.joshi.1505@gmail.com>
2025-05-11 23:04:09 +0530

    simple addition

--- pages/Homepage/Hero section/herosection.jsx
+++ pages/Homepage/Hero section/herosection.jsx
@@ -1,14 +1,20 @@
+"use client"
 import "./Hero.css";
+import { useRouter } from "next/navigation"
 
 export default function Hero() {
+  const router = useRouter();
+    const handleSignUp= () => {
+      router.push("/SignUpPage"); 
+    }
   return (
   <div className="hero">
   <h1>Empowering Taxpayers. Building the Nation.</h1>
   <p>Seamless and Transparent Solutions for Every Indian</p>
   <p className="description">EZY Tax is your next-generation platform designed to simplify tax filing, maximize your savings, and ensure complete peace of mind.</p>
   <div className="hero-buttons">
-  <button className="primary-btn">Get Started</button>
-  <a href="#Services"><button className="secondary-btn">Learn More</button></a>
+  <button onClick={handleSignUp} className="primary-btn">Get Started</button>
+  <button className="secondary-btn">Learn More</button>
   </div>
 </div>
   );

c9fef84034d83a83655728fcfb81ad44cd2334a8
Hashicy <harshita.joshi.1505@gmail.com>
2025-07-20 22:26:31 +0530

    added ai agent

--- src/app/page.js
+++ src/app/page.js
@@ -11,6 +11,7 @@ export default function Home() {
   const [page,pageupd]=useState("Home")
   return (
     <>
+    <script src="//code.tidio.co/h286lno4l1f0ibu9qf0f633rgijfic0u.js" async></script>
     <Header pageupd={pageupd}/>
     {page=="Home"?<Homepage/>:page=="AboutUs"?<AboutPage/>:page=="Blogs"?<BlogsPage/>:<ProductPage/>}
     <Footer pageupd={pageupd}/>
